a05d24a771e27a74c1ad2c2405635a0822c99211
Evan Hensleigh <futuraprime@gmail.com>
2014-10-24 22:32:24 -0400

    Started down the oscillator path.

--- js/main.js
+++ js/main.js
@@ -2,103 +2,46 @@
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 
-// W053: allow string constructors
-/* jshint -W053 */
-// first, for simplicity:
-var C   = new String('C');
-var Bs  = C;
-var Cs  = new String('C#');
-var D   = new String('D');
-var Ds  = new String('D#');
-var E   = new String('E');
-var F   = new String('F');
-var Es  = F;
-var Fs  = new String('F#');
-var G   = new String('G');
-var Gs  = new String('G#');
-var A   = new String('A');
-var As  = new String('A#');
-var B   = new String('B');
-/* jshint +W053 */
+var wholeNote = 0.75;
 
-C.next  = Cs;
-Cs.next = D;
-D.next  = Ds;
-Ds.next = E;
-E.next  = F;
-F.next  = Fs;
-Fs.next = G;
-G.next  = Gs;
-Gs.next = A;
-A.next  = As;
-As.next = B;
+// steps is steps from A4
+function ntf(steps) {
+  return 440 * Math.pow(2, (steps/12));
+}
 
-Cs.prev = C;
-D.prev  = Cs;
-Ds.prev = D;
-E.prev  = Ds;
-F.prev  = E;
-Fs.prev = F;
-G.prev  = Fs;
-Gs.prev = G;
-A.prev  = Gs;
-As.prev = A;
-B.prev  = As;
+function playFrequency(frequency, duration, offset) {
+  offset = offset || 0;
+  var oscillator = context.createOscillator();
+  var time = context.currentTime + offset;
 
+  oscillator.frequency.value = frequency;
+  oscillator.connect(context.destination);
 
-var wholeNote = 2; // seconds
-function generateNote(note, octave, size) {
+  oscillator.noteOn(time);
+  oscillator.noteOff(time + duration);
+}
+
+var C  = ntf(-9);
+var Cs = ntf(-8);
+var D  = ntf(-7);
+var Ds = ntf(-6);
+var E  = ntf(-5);
+var F  = ntf(-4);
+var Fs = ntf(-3);
+var G  = ntf(-2);
+var Gs = ntf(-1);
+var A  = ntf( 0);
+var As = ntf( 1);
+var B  = ntf( 2);
+
+function parseNote(note, octave, duration) {
   if(note instanceof Array) {
-    size = note[2];
+    duration = note[2];
     octave = note[1];
     note = note[0];
   }
-  var blob = Synth.generate('piano', note, octave, size * wholeNote);
-  var reader = new FileReader();
-  var buffer = null;
-  reader.addEventListener('loadend', function() {
-    context.decodeAudioData(reader.result, function(b) {
-      buffer = b;
-    });
-  });
-  reader.readAsArrayBuffer(blob);
-  var fn = function(time) {
-    time = time === undefined ? 0 : time;
-    var source = context.createBufferSource();
-    source.buffer = buffer;
-    source.connect(context.destination);
-
-    source.start(time);
-  };
-  fn.size = size;
-
-  return fn;
-}
-
-function declineNote(note, steps) {
-  note = note.slice();
-  for(var i=0;i<steps;++i) {
-    if(note[0].prev) {
-      note[0] = note[0].prev;
-    } else {
-      note[0] = B;
-      note[1] -= 1;
-    }
-  }
-  return note;
-}
-
-function ascendNote(note, steps) {
-  note = note.slice();
-  for(var i=0;i<steps;++i) {
-    if(note[0].next) {
-      note[0] = note[0].next;
-    } else {
-      note[0] = C;
-      note[1] += 1;
-    }
-  }
-  return note;
+  var frequency = note * Math.pow(2, octave - 4);
+  return [frequency, duration];
 }
 
 var BWV1074 = ([
@@ -111,37 +54,43 @@ var BWV1074 = ([
   [B, 3, 0.75 ],
   [A, 3, 0.25 ],
   [B, 3, 0.25 ]
-]);
-var inC = BWV1074.map(generateNote);
+]).map(parseNote);
+
+function bendNote(note, steps) {
+  return [
+    note[0] * Math.pow(2, (steps/12)),
+    note[1]
+  ];
+}
+
+var inC = BWV1074.map(function(note) { return note; });
 var inG = BWV1074.map(function(note) {
-  return declineNote(note, 4);
-}).map(generateNote);
+  return bendNote(note, -4);
+});
 var inA = BWV1074.map(function(note) {
-  return ascendNote(note, 3);
-}).map(generateNote);
+  return bendNote(note, 3);
+});
 var inD = BWV1074.map(function(note) {
-  return declineNote(note, 10);
-}).map(generateNote);
+  return bendNote(note, -10);
+});
 
 function canon(loop, repetitions, delay) {
   delay = delay === undefined ? 0 : delay;
   repetitions = repetitions === undefined ? 1 : repetitions;
   var time = context.currentTime + delay;
-  console.log(delay, time);
-  var n = 1;
   var size = 0;
   for(var j=0;j<repetitions;++j) {
     for(var i=0,l=loop.length;i<l;++i) {
-      loop[i](time + n * size);
-      size += loop[i].size;
+      playFrequency(loop[i][0], loop[i][1], size + delay);
+      size += loop[i][1];
     }
   }
 }
 
 var play = document.getElementById('play');
 play.addEventListener('click', function() {
-  canon(inG, 3);
-  canon(inC, 3, 0.5);
-  canon(inA, 3, 1);
-  canon(inD, 3, 1.5);
+  canon(inG, 2);
+  canon(inC, 2, 0.5);
+  canon(inA, 2, 1);
+  canon(inD, 2, 1.5);
 });
